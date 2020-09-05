1d475b940ef45982144ce7d0346fc1497e90d0f1
sid2021 <wolanski.jakub@gmail.com>
2020-09-05 16:15:17 +0200

    Updates

---
+++ precache-manifest.5d69ade7816f11dc33ee8c9f67207ee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be47278e0533fc3ab4cfcde5057a03d5",
+    "url": "/Drum-Machine/index.html"
+  },
+  {
+    "revision": "99dad0743e3a6f57902b",
+    "url": "/Drum-Machine/static/css/main.1bec123c.chunk.css"
+  },
+  {
+    "revision": "6e1091d4bde52b9dee7b",
+    "url": "/Drum-Machine/static/js/2.eb2cb1bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Drum-Machine/static/js/2.eb2cb1bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99dad0743e3a6f57902b",
+    "url": "/Drum-Machine/static/js/main.261a8be2.chunk.js"
+  },
+  {
+    "revision": "fb27c42abcd2a828e8a6",
+    "url": "/Drum-Machine/static/js/runtime-main.50eb1de8.js"
+  }
+]);
